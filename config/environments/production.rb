f708b11e77776b510cb4ad2e64dc4690cdb3bf1e
Hendra Gunawan <hengun.bless@gmail.com>
2013-01-10 00:33:52 +0700

    add assets precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Schedule::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
